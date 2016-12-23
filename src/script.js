5329e7810bcba0e1f7e3c9b604bfa9978ce98249
kasari <kasari3939@gmail.com>
2016-12-23 22:49:01 +0900

    react sample

--- src/script.js
+++ src/script.js
@@ -1,14 +1,40 @@
-import Person from './person';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-class Friend extends Person{
-    constructor(name) {
-      super(name);
-    }
-    writeName() {
-      document.write(`My name is ${this.name}.`);
-    }
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
-var friend = new Friend('hi');
-
-friend.writeName();
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('root')
+);
