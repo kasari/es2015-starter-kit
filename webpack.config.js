104aecf2f460929e3be1b1f12ca45b98d85bb49e
kasari <kasari3939@gmail.com>
2016-12-23 16:21:51 +0900

    webpackの設定

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    context: __dirname + '/src',
+    entry: {
+        javascript: './script.js',
+        html: './index.html'
+    },
+
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        contentBase: 'dist',
+        port: 3000
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                query:{
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.html$/,
+                loader: 'file?name=[path][name].[ext]'
+            }
+        ]
+    }
+};
