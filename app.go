7068c18fbf4aba008a7a9e31a22611b4a6911452
kasari <kasari3939@gmail.com>
2016-12-26 19:46:34 +0900

    player一覧もfetchする

--- app.go
+++ app.go
@@ -17,18 +17,47 @@ type Player struct {
 	Email string `json:"email"`
 }
 
+var (
+	players   = []Player{}
+	playerMap = make(map[int]Player)
+)
+
+func playerDataInit() {
+	players = []Player{
+		Player{1, "asari", "asari@gmail.com"},
+		Player{2, "kasari", "kasari@docomo.ne.jp"},
+		Player{10, "vongole", "vongole@yahoo.co.jp"},
+	}
+
+	for _, p := range players {
+		playerMap[p.ID] = p
+	}
+}
+
+func getPlayers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	bytes, err := json.Marshal(players)
+	if err != nil {
+		fmt.Fprintln(w, err)
+	}
+	fmt.Fprintln(w, string(bytes))
+}
+
 func getPlayer(c web.C, w http.ResponseWriter, r *http.Request) {
 	playerID, err := strconv.Atoi(c.URLParams["player_id"])
-	if playerID == 0 || err != nil {
+	if err != nil {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
-	player := &Player{
-		ID:    playerID,
-		Name:  fmt.Sprintf("player_%d", playerID),
-		Email: fmt.Sprintf("player_%d@gmail.com", playerID),
+	player, ok := playerMap[playerID]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
 	}
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	bytes, err := json.Marshal(player)
 	if err != nil {
@@ -38,6 +67,9 @@ func getPlayer(c web.C, w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	playerDataInit()
+
+	goji.Get("/players", getPlayers)
 	goji.Get("/players/:player_id", getPlayer)
 
 	flag.Set("bind", ":8080")
--- src/js/players.js
+++ src/js/players.js
@@ -2,13 +2,35 @@ import React from 'react'
 import {Link} from 'react-router'
 
 class Players extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: []
+    };
+
+    fetch("http://localhost:8080/players")
+      .then((response) => response.json())
+      .then((json) => {
+        console.dir(json);
+        this.setState({
+          players: json
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   render() {
     return (
       <div>
         <h1>This is Players page!</h1>
         <ul>
-          <li><Link to="/players/39">player 39</Link></li>
-          <li><Link to="/players/10000">player 10000</Link></li>
+          {
+            this.state.players.map((player) => {
+              return <li key={player.id}><Link to={`/players/${player.id}`}>{player.name}</Link></li>
+            })
+          }
         </ul>
       </div>
     )
