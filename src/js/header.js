21cc3531e0800145e404eb7058bf96eca38c49b1
kasari <kasari3939@gmail.com>
2016-12-26 21:25:56 +0900

    headerにbootstrapを使う

--- src/js/header.js
+++ src/js/header.js
@@ -1,17 +1,33 @@
 import React from 'react'
 import {Link} from 'react-router'
+import Nav from 'react-bootstrap/lib/Nav';
+import Navbar from 'react-bootstrap/lib/Navbar';
+import NavDropdown from 'react-bootstrap/lib/NavDropdown';
+import NavItem from 'react-bootstrap/lib/NavItem';
+import MenuItem from 'react-bootstrap/lib/MenuItem';
+import { LinkContainer } from 'react-router-bootstrap';
 
 class Header extends React.Component {
   render() {
     return (
-      <header>
-        <nav>
-          <ul>
-            <li><Link to="/">home</Link></li>
-            <li><Link to="/players">players</Link></li>
-          </ul>
-        </nav>
-      </header>
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">home</Link>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <LinkContainer to="/players">
+            <NavItem>players</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/hoge">
+            <NavItem>hoge</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/fuga">
+            <NavItem>fuga</NavItem>
+          </LinkContainer>
+        </Nav>
+      </Navbar>
     )
   };
 }
